422d5512204b7d7fadf0996a5b0b7bde2f706dad
harshvekariya01 <vekariyaharsh01@gmail.com>
2023-08-31 10:55:33 +0530

    change code

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -77,22 +77,30 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio_db',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432',
+
+# server_type = 'localhost'
+server_type = 'live'
+
+if server_type == 'localhost':
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'portfolio_db',
+            'USER': 'postgres',
+            'PASSWORD': '12345678',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
